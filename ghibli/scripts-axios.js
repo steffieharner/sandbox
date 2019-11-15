8f6a45cf90cfda2cc8535c9fcdcc67917e6d5674
steffieharrner <steffie.harner@gmail.com>
2019-11-15 13:38:43 +0900

    axios and XML complete

--- ghibli/scripts-axios.js
+++ ghibli/scripts-axios.js
@@ -8,25 +8,24 @@ const container = document.createElement("div");
 container.setAttribute("class", "container");
 app.appendChild(container);
 
-var request = new XMLHttpRequest();
-request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+// we do not const axios = require('axios') here because
+// it doesn't work in the browser, instead use a CDN in the script
+
+axios.get("https://ghibliapi.herokuapp.com/films").then(res => {
+  // handle success
+  if (res.status >= 200 && res.status < 400) {
+    res.data.forEach(movie => {
       const card = document.createElement("div");
       card.setAttribute("class", "card");
+      container.appendChild(card);
 
       const h1 = document.createElement("h1");
       h1.textContent = movie.title;
+      card.appendChild(h1);
 
       const p = document.createElement("p");
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
       card.appendChild(p);
     });
   } else {
@@ -34,6 +33,4 @@ request.onload = function() {
     errorMessage.textContent = `Gah, it's not working!`;
     app.appendChild(errorMessage);
   }
-};
-
-request.send();
+});
